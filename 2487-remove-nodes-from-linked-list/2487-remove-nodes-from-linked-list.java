c388f3100626b3fe04e8b68d69a95e50e32bcfb8
Shivam singh <shivamasphion@gmail.com>
2026-08-30 23:03:41 +0530

    Time: 18 ms (48.5%), Space: 136 MB (14.36%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -9,22 +9,40 @@
  * }
  */
 class Solution {
-    // private ListNode reverse(ListNode head){
-    //     ListNode newHead=reverse(head);
-    //     ListNode front=head.next;
-    //     front.next=head;
-    //     head.next=null;
-    //     return newHead;
-    // }
+    public ListNode reverse(ListNode head){
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode newHead=reverse(head.next);
+       ListNode front=head.next;
+         front.next=head;
+         head.next=null;
+        return newHead;
+    }
     public ListNode removeNodes(ListNode head) {
-      if(head==null||head.next==null){
-        return head;
-      }
-      ListNode nxt=removeNodes(head.next);
-       if(head.val<nxt.val){
-        return nxt;
-       }
-       head.next=nxt;
-       return head;
+    //   if(head==null||head.next==null){
+    //     return head;
+    //   }
+    //   ListNode nxt=removeNodes(head.next);
+    //    if(head.val<nxt.val){
+    //     return nxt;
+    //    }
+    //    head.next=nxt;
+    //    return head;
+    head=reverse(head);
+    int max=head.val;
+    ListNode curr=head;
+    while(curr.next!=null){
+        if(curr.next.val<max){
+            curr.next=curr.next.next;
+
+        }
+        else{
+           curr=curr.next;
+           max=curr.val;
+        }
+    }
+    head=reverse(head);
+    return head;
     }
 }
